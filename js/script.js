e430de9ff45745754805eb812eaf628f465b6b70
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-09 14:10:53 -0600

    Completed CSS animation

--- js/script.js
+++ js/script.js
@@ -12,4 +12,42 @@ let htmlSpans = textRain.innerText
 
 textRain.innerHTML = htmlSpans;
 
-//console.log(htmlSpans);
+//hide greeting message
+function hideMessage() {
+  document.getElementsByTagName('h3')[0].style.display = 'none';
+  document.getElementsByClassName('lightbar')[0].style.display = 'none';
+}
+setTimeout(hideMessage, 10000);
+
+//hide raining
+function hideRaining() {
+  document.getElementsByClassName('rain')[0].style.backgroundImage = "url('')";
+}
+setTimeout(hideRaining, 12000);
+
+//hide raining alert message
+function hideAlertMessage() {
+  document.getElementsByTagName('h2')[0].style.display = 'none';
+}
+setTimeout(hideAlertMessage, 13000);
+
+//show raining
+function showRaining() {
+  document.getElementsByClassName('rain')[0].style.backgroundImage =
+    "url('./images/rain.png')";
+}
+setTimeout(showRaining, 17000);
+
+//show raining alert message
+function showAlertMessage() {
+  document.getElementsByTagName('h2')[0].style.display = 'block';
+}
+setTimeout(showAlertMessage, 18000);
+
+setTimeout(hideRaining, 22000);
+setTimeout(hideAlertMessage, 23000);
+setTimeout(showRaining, 27000);
+setTimeout(showAlertMessage, 28000);
+
+setTimeout(showRaining, 35000);
+setTimeout(showAlertMessage, 36000);
